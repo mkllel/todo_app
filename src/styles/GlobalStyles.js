6a1bc857c07feafccbce4f6d98a84d2fb2d9e233
MKLEE <73101332+mkllel@users.noreply.github.com>
2024-06-17 11:46:21 +0900

    Update GlobalStyles.js

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,13 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'iceHimchan-Rg';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2307-2@1.0/iceHimchan-Rg.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
   body {
-    font-family: Arial, sans-serif;
+    font-family: 'iceHimchan-Rg', cursive;
     background-color: cornflowerblue;
     display: flex;
     justify-content: center;
